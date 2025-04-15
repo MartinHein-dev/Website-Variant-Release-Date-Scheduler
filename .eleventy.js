dbaa2be760bde6a50a393d8d18328b4a6c28f167
MartinHein-dev <martinhein86@gmx.de>
2025-04-15 09:20:37 +0200

    Add favicon and update layout to include it

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/fontawesome");
+  eleventyConfig.addPassthroughCopy("src/images");
 
   // WICHTIG: Erstellt eine .nojekyll-Datei, damit GitHub Pages die Dateien nicht mit Jekyll verarbeitet
   eleventyConfig.addPassthroughCopy({ "src/_github/.nojekyll": ".nojekyll" });
